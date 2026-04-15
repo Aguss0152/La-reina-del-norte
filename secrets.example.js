4766c792e718394f74dc4b72d130cca437c2cdff
Aguss0152 <47722510+Aguss0152@users.noreply.github.com>
2026-04-15 08:50:05 -0300

    Eliminar secrets.example.js

--- secrets.example.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-    SHEET_URL: 'https://docs.google.com/spreadsheets/d/REPLACE_WITH_YOUR_SHEET_ID/export?format=csv',
-    GOOGLE_MAPS_API_KEY: 'REPLACE_WITH_YOUR_GOOGLE_MAPS_API_KEY'
-};
