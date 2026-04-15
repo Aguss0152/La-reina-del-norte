ff51aa172a80d57913833dd1202ae0f619ce9b7f
Aguss0152 <thewolf0152@gmail.com>
2026-04-15 01:24:55 -0300

    Add secrets.js for deployment

---
+++ secrets.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+    SHEET_URL: 'https://docs.google.com/spreadsheets/d/1k5n4jeqsf9gxHrCm8aTEFYmK3B_sakl0YXts6O-i2DU/export?format=csv',
+    GOOGLE_MAPS_API_KEY: 'AIzaSyA3XEP3NsM91gadzvwqBz1oBGwqoGqtcFQ'
+};
